7678174357c18ef287ef527fa24932bc88ffed77
SummerBetter <hxun1213@163.com>
2025-12-23 16:34:27 +0800

    Initial commit: Java project
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/hxun/stockstorm_backend/StockStormBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hxun.stockstorm_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockStormBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockStormBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hxun/stockstorm_backend/StockStormBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hxun.stockstorm_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockStormBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
