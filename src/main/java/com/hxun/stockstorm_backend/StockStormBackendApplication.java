ab206d5e2ec5230c81614e2138072d871b51ed7d
SummerBetter <hxun1213@163.com>
2025-12-30 15:54:51 +0800

    feat(LoginController):新增sa-token框架的登录认证功能
    登录认证功能：测试阶段，不可投入使用

--- src/main/java/com/hxun/stockstorm_backend/StockStormBackendApplication.java
+++ src/main/java/com/hxun/stockstorm_backend/StockStormBackendApplication.java
@@ -1,5 +1,6 @@
 package com.hxun.stockstorm_backend;
 
+import cn.dev33.satoken.SaManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class StockStormBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StockStormBackendApplication.class, args);
+        System.out.println(SaManager.getConfig());
     }
 
 }
